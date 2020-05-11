58236b2db879c3f0799d1da9e61125a7475ba81c
Cassandra Sutton <cassandrasutton@gmail.com>
2020-05-11 16:31:20 -0700

    modified files

--- desserts.py
+++ desserts.py
@@ -8,6 +8,46 @@ class Cupcake:
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+    
+    def __init__(self, name, flavor, price, qty=0):
+            """Names a cupcake with flavor, price and qty attributes."""
+            self.name = name
+            self.flavor = flavor
+            self.price = price
+            self.qty = qty
+
+    def add_stock(self, amount):
+        """Adds existing stock amount to qty of cupcakes."""
+        stock = amount + self.qty
+        self.qty = stock 
+
+    def sell(self, amount):
+        """Sells a number of cupcakes."""
+        if amount <= self.qty:
+            stock = self.qty - amount
+            self.qty = stock
+        else:
+            print("Sorry, these cupcakes are sold out")  
+
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+        """Scale the list of ingredients by the given amount of cupcakes."""  
+        for ingredient in (ingredients):
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
@@ -23,9 +63,4 @@ if __name__ == '__main__':
         print('ALL TESTS PASSED')
 
 
-    def __init__(self, name, flavor, price, qty):
-        """Names a cupcake with flavor, price and qty attributes."""
-        self.name = name
-        self.flavor = flavor
-        self.price = price
-        self.qty = qty
+    
